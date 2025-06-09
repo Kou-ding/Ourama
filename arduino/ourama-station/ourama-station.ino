99828260a0619a6acc83abbecf80eb29d00a1fa0
Kou-ding <fotispapadak@gmail.com>
2025-06-09 20:45:35 +0300

    Refactor ourama-station code to remove unused network functionality and improve readability

--- arduino/ourama-station/ourama-station.ino
+++ arduino/ourama-station/ourama-station.ino
@@ -1,52 +1,43 @@
-/*
-Arduino Wireless Communication Tutorial,
-Example 1 - Receiver Code,
-,
-by Dejan Nedelkovski, www.HowToMechatronics.com,
-,
-Library: TMRh20/RF24, https://github.com/tmrh20/RF24/,
-*/
-
 #include <SPI.h>
-#include <RF24Network.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(9, 10); // CE, CSN
-RF24Network network(radio);// Include the radio in the network
-const uint16_t this_node = 00;   // Address of our node in Octal format 
+// Creates an RF24 object
+RF24 radio(9, 10); // 9 -> CE (Chip Enable pin), 10 -> CSN (Chip Select Not pin)
 
-//const byte address[6] = "00001";
+// This is the pipe address
+const byte address[6] = "00001";
 
 void setup() {
-  Serial.begin(9600); 
-  radio.begin(); 
-  network.begin(108, this_node); //(channel, node address)
-  //radio.setChannel(108); // Match transmitter
-  radio.setDataRate(RF24_250KBPS); // Match transmitter radio.setPALevel(RF24_PA_MIN);
-  radio.setPALevel(RF24_PA_MIN); // Match transmitter (optional, but good for consistency)
-  radio.enableDynamicPayloads(); 
-  //radio.openReadingPipe(0, address); 
-  //radio.startListening(); 
+  // Initializes serial communication at 9600 baud
+  Serial.begin(9600);
+  // Initializes the nRF24L01 radio
+  radio.begin();
+  // Opens reading pipe 0 using the specified address
+  radio.openReadingPipe(0, address);
+  // Sets transmission power level to minimum (for short-range)
+  radio.setPALevel(RF24_PA_MIN);
+  // Sets data rate to 1 Megabit per second.
+  radio.setDataRate(RF24_250KBPS); // Better range and reliability
+  // Sets RF channel (frequency = 2.408 GHz)
+  radio.setChannel(108);
+  // Disable the radio auto-acknowledgment
+  radio.setAutoAck(true);
+  radio.enableDynamicPayloads();
+  // Puts the nRF24L01 module into receive mode
+  radio.startListening();
 }
 
 void loop() {
-
-  network.update();  // Process incoming network traffic
-
-  while ( network.available() ) {     // If there's any data
-    RF24NetworkHeader header;
-    char text[40] = "";
-
-    network.read(header, &text, sizeof(text)); // Read the data packet
-    Serial.println(text); 
+  // Declares a buffer receivedUID to store up to 24 characters + null terminator
+  char receivedUID[25]={0};
+  // Checks if data is available from the radio module
+  if (radio.available())  {
+    // Reads the incoming data into the buffer
+    radio.read(&receivedUID, sizeof(receivedUID));
+    // Prints the received UID to the Serial Monitor
+    Serial.println(receivedUID);
+    // Clears the RX buffer to avoid reading the same message multiple times
+    //radio.flush_rx(); // Might interfere with with the ACK
   }
-  delay(200);
-
-  // if (radio.available()) { 
-  //   char text[40] = ""; 
-  //   radio.read(&text, sizeof(text)); 
-  //   Serial.println(text); 
-  // } 
-  // delay(200);
 }
